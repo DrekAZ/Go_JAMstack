4786dbf79b460295534e9f77a86c34b8f1b9a3ee
A <kassuruv2@gmail.com>
2020-08-25 15:42:18 +0900

    add login feature

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -14,14 +14,4 @@ func TestLogin(t *testing.T) {
 	err = Login(ctx, fireClient, "a@a.com")
 	t.Log(err)
 
-	/*t.Run("exist_email", func(t *testing.T) {
-		t.Log("exist_email")
-		_ = Login(ctx, fireClient, "kassuruv2@gmail.com")
-	})
-
-	t.Run("not_exist_email", func(t *testing.T) {
-		t.Log("not_exist_email")
-		_ = Login(ctx, fireClient, "k2@gmail.com")
-	})*/
-
 }
